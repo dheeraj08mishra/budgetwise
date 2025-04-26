db983a48c6b4a3537f7b47e387be029ea9918b8e
Dheeraj Mishra <dheeraj997mishra@gmail.com>
2025-04-27 01:22:15 +0530

    initial commit with firebase user authentication

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Body from "./Body";
+
+const App = () => {
+  return (
+    <>
+      <Body />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Login from "./Login";
+
+const Layout = () => {
+  return (
+    <>
+      <Login />
+    </>
+  );
+};
+
+export default Layout;
