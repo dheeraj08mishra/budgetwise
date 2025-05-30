cedb72429072e9965d5ac3855f4f12b418f9bece
Dheeraj Mishra <dheeraj997mishra@gmail.com>
2025-05-30 22:13:10 +0530

    logout build fixed

--- src/utils/redux/profileSlice.jsx
+++ src/utils/redux/profileSlice.jsx
@@ -1,5 +1,4 @@
-import { createSlice, current } from "@reduxjs/toolkit";
-import { logout } from "./userSlice";
+import { createSlice } from "@reduxjs/toolkit";
 
 const profileSlice = createSlice({
   name: "profile",
